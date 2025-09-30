5946787f4de1df91fcdae31a2cc90a77379aec1b
m-wilpert <148054865+m-wilpert@users.noreply.github.com>
2025-09-30 23:03:55 +0200

    Update flake.nix to include desktop config

--- flake.nix
+++ flake.nix
@@ -28,6 +28,13 @@
           inputs.home-manager.nixosModules.default
         ];
       };
+      desktop = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/desktop/configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
     }; 
   };
 }
